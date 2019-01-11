2ac9a122b56fda711cedf4525ac1b40eb9ee507a
Luis Zabala <luis.zabala076@gmail.com>
2019-01-11 00:19:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,3 +12,16 @@ else
 end
 end
 
+def hello_t(array)
+  if block_given?
+    i = 0
+    
+    while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+else 
+  puts "Hey! No block given!"
+end
+end
